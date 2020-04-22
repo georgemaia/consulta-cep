d77a31bc58537600d9dd34f854de78c6abf9acd6
georgemaia <georgemaia@gmail.com>
2020-04-21 21:31:47 -0300

    correct class with psr12 in phpcs

--- src/Search.php
+++ src/Search.php
@@ -4,13 +4,13 @@ namespace Georgemaia\ConsultaCep;
 
 class Search
 {
-    private $_url = "http://viacep.com.br/ws/";
+    private $url = "http://viacep.com.br/ws/";
 
     public function getAddressFromZipcode(string $zipCode): array
     {
-        $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);  
+        $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
 
-        $get = file_get_contents($this->_url . $zipCode . "/json");
+        $get = file_get_contents($this->url . $zipCode . "/json");
 
         return (array) json_decode($get);
     }
